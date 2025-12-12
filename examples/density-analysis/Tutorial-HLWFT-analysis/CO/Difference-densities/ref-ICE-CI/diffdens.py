8630892e0aea45ad54afa8415209e8f42293d8a3
Louie Slocombe <louies@hotmail.co.uk>
2025-12-12 14:29:53 -0700

    removed files

--- examples/density-analysis/Tutorial-HLWFT-analysis/CO/Difference-densities/ref-ICE-CI/diffdens.py
+++
@@ -1,6 +0,0 @@
-from ash import *
-
-moldenfiles=glob.glob("*molden*")
-
-diffdens_tool(reference_orbfile="ICE_CI_mp2nat_tgen_1e-06.molden",
-    dir='.', grid=3, printlevel=2)
--- examples/metadynamics/2CV-3FGABA/continuum-solvent/get_free_energy.py
+++
@@ -1,8 +0,0 @@
-from ash import *
-
-
-biasdir='./biasdirectory'
-
-# Free-energy plot
-metadynamics_plot_data(biasdir=biasdir, dpi=200, imageformat='png')
-
