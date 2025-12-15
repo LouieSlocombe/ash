550026ec77868799bd13f9503cb0997d8bea694e
Louie Slocombe <louies@hotmail.co.uk>
2025-12-15 16:06:05 -0700

    added test_openmm_md

--- tests/test_openmm.py
+++ tests/test_openmm.py
@@ -48,3 +48,41 @@ def test_openmm_modeller():
     os.remove('system_afterH.pdb')
     os.remove('system_aftersolvent_ions.pdb')
     os.remove('system_full.xml')
+
+
+def test_openmm_md():
+    # Read raw PDB-file, fix using pdbfixer, setup using Modeller and run MD
+    pdb_file = f"./tests/pdbfiles/1aki.pdb"
+
+
+    frag = Fragment(pdbfile=pdb_file)
+
+    # # OpenMM object
+    # omm = OpenMMTheory(xmlfiles=["amber14-all.xml", "amber14/tip3pfb.xml"], pdbfile=pdb_file, periodic=True,
+    #                    autoconstraints='HBonds', rigidwater=True)
+
+
+    # Setting up new system, adding hydrogens, solvent, ions and defining forcefield, topology
+    omm, fragment = OpenMM_Modeller(pdbfile=pdb_file,
+                                    forcefield='Amber14',
+                                    watermodel="tip3p-fb",
+                                    pH=7.0,
+                                    solvent_padding=10.0,
+                                    ionicstrength=0.1,
+                                    platform='CPU')
+
+    OpenMM_MD(fragment=fragment,
+              theory=omm,
+              simulation_steps=100,
+              traj_frequency=2,
+              trajectory_file_option='PDB',
+              )
+    os.remove('ASH_SP.result')
+    os.remove('finalsystem.pdb')
+    os.remove('finalsystem.xyz')
+    os.remove('finalsystem.ygg')
+    os.remove('system_afterfixes.pdb')
+    os.remove('system_afterfixes2.pdb')
+    os.remove('system_afterH.pdb')
+    os.remove('system_aftersolvent_ions.pdb')
+    os.remove('system_full.xml')
